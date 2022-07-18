1efeabd856ccd37da7897ce5201bc122f88422a2
Aaron Deas <deas.aaron@gmail.com>
2022-07-18 18:49:43 -0400

    Added helpers to template render functino

--- src/controllers/homeRoutes.ts
+++ src/controllers/homeRoutes.ts
@@ -2,7 +2,18 @@ import {Router, Request, Response, ErrorRequestHandler} from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.render('home', {layout: false});
+  res.render('home', {
+    layout: false,
+    showTitle: true,
+    helpers: {
+      foo() {
+        return 'foo.';
+      },
+      bar() {
+        return 'This is the bar() function';
+      },
+    },
+  });
 });
 
 export default router;
